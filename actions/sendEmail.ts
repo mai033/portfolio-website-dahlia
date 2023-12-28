c3908294e2c67cdba0c1f708263221b48cac0005
mai033 <145622463+mai033@users.noreply.github.com>
2023-12-27 21:52:38 -0800

    added clear form

--- actions/sendEmail.ts
+++ actions/sendEmail.ts
@@ -1,3 +1,53 @@
+// 'use server';
+
+// import React from 'react';
+// import { Resend } from 'resend';
+// import { validateString, getErrorMessage } from '@/lib/utils';
+// import ContactFormEmail from '@/email/contact-form-email';
+
+// const resend = new Resend(process.env.RESEND_API_KEY);
+
+// export const sendEmail = async (formData: FormData) => {
+//   const senderEmail = formData.get('senderEmail');
+//   const message = formData.get('message');
+
+//   console.log(senderEmail);
+
+//   if (!validateString(senderEmail, 500)) {
+//     return {
+//       error: 'Invalid sender email',
+//     };
+//   }
+
+//   if (!validateString(message, 5000)) {
+//     return {
+//       error: 'Invalid message',
+//     };
+//   }
+
+//   let data;
+//   try {
+//     data = await resend.emails.send({
+//       from: 'Contact Form <onboarding@resend.dev>',
+//       to: 'dahliamai033@gmail.com',
+//       subject: 'Message from contact form',
+//       reply_to: senderEmail as string,
+//       react: React.createElement(ContactFormEmail, {
+//         message: message as string,
+//         senderEmail: senderEmail as string,
+//       }),
+//     });
+//   } catch (error: unknown) {
+//     return {
+//       error: getErrorMessage(error),
+//     };
+//   }
+
+//   return {
+//     data,
+//   };
+// };
+
 'use server';
 
 import React from 'react';
@@ -7,12 +57,13 @@ import ContactFormEmail from '@/email/contact-form-email';
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
-export const sendEmail = async (formData: FormData) => {
-  const senderEmail = formData.get('senderEmail');
-  const message = formData.get('message');
-
-  console.log(senderEmail);
-
+export const sendEmail = async ({
+  senderEmail,
+  message,
+}: {
+  senderEmail: string;
+  message: string;
+}) => {
   if (!validateString(senderEmail, 500)) {
     return {
       error: 'Invalid sender email',
@@ -31,10 +82,10 @@ export const sendEmail = async (formData: FormData) => {
       from: 'Contact Form <onboarding@resend.dev>',
       to: 'dahliamai033@gmail.com',
       subject: 'Message from contact form',
-      reply_to: senderEmail as string,
+      reply_to: senderEmail,
       react: React.createElement(ContactFormEmail, {
-        message: message as string,
-        senderEmail: senderEmail as string,
+        message: message,
+        senderEmail: senderEmail,
       }),
     });
   } catch (error: unknown) {
--- components/contact.tsx
+++ components/contact.tsx
@@ -1,6 +1,77 @@
 'use client';
 
-import React from 'react';
+// import React from 'react';
+// import SectionHeading from './section-heading';
+// import { motion } from 'framer-motion';
+// import { useSectionInView } from '@/lib/hooks';
+// import { sendEmail } from '@/actions/sendEmail';
+// import SubmitBtn from './submit-btn';
+// import toast from 'react-hot-toast';
+
+// export default function Contact() {
+//   const { ref } = useSectionInView('Contact', 0.3);
+
+//   return (
+//     <motion.section
+//       ref={ref}
+//       id="contact"
+//       className="mb-20 sm:mb-28 w-[min(100%,38rem)] text-center"
+//       initial={{
+//         opacity: 0,
+//       }}
+//       whileInView={{
+//         opacity: 1,
+//       }}
+//       transition={{
+//         duration: 1,
+//       }}
+//       viewport={{
+//         once: true,
+//       }}
+//     >
+//       <SectionHeading>Contact</SectionHeading>
+//       <p className="text-gray-700 -mt-6 dark:text-white/80">
+//         Feel free to get in touch with me directly at{' '}
+//         <a className="underline" href="mailto:dahliamai033@gmail.com">
+//           dahliamai033@gmail.com
+//         </a>{' '}
+//         or simply drop me a message using this form. I'm always happy to
+//         connect!
+//       </p>
+
+//       <form
+//         className="mt-10 flex flex-col dark:text-black-"
+//         action={async (formData) => {
+//           const { data, error } = await sendEmail(formData);
+//           if (error) {
+//             toast.error(error);
+//             return;
+//           }
+//           toast.success('Email sent successfully!');
+//         }}
+//       >
+//         <input
+//           className="h-14 px-4 rounded-lg borderBlack dark:bg-white dark:bg-opacity-90 dark:focus:bg-opacity-100 dark:text-black transition-all dark:outline-none"
+//           name="senderEmail"
+//           type="email"
+//           required
+//           maxLength={500}
+//           placeholder="Your email"
+//         />
+//         <textarea
+//           className="h-52 my-3 rounded-lg borderBlack p-4 dark:bg-white dark:bg-opacity-90 dark:focus:bg-opacity-100 dark:text-black transition-all dark:outline-none"
+//           name="message"
+//           placeholder="Message"
+//           required
+//           maxLength={5000}
+//         />
+//         <SubmitBtn />
+//       </form>
+//     </motion.section>
+//   );
+// }
+
+import React, { useState } from 'react';
 import SectionHeading from './section-heading';
 import { motion } from 'framer-motion';
 import { useSectionInView } from '@/lib/hooks';
@@ -10,6 +81,26 @@ import toast from 'react-hot-toast';
 
 export default function Contact() {
   const { ref } = useSectionInView('Contact', 0.3);
+  const [senderEmail, setSenderEmail] = useState('');
+  const [message, setMessage] = useState('');
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    const formData = {
+      senderEmail,
+      message,
+    };
+    const { data, error } = await sendEmail(formData);
+    if (error) {
+      toast.error(error);
+      return;
+    }
+    toast.success('Email sent successfully!');
+
+    // Clear the input fields
+    setSenderEmail('');
+    setMessage('');
+  };
 
   return (
     <motion.section
@@ -41,14 +132,7 @@ export default function Contact() {
 
       <form
         className="mt-10 flex flex-col dark:text-black-"
-        action={async (formData) => {
-          const { data, error } = await sendEmail(formData);
-          if (error) {
-            toast.error(error);
-            return;
-          }
-          toast.success('Email sent successfully!');
-        }}
+        onSubmit={handleSubmit}
       >
         <input
           className="h-14 px-4 rounded-lg borderBlack dark:bg-white dark:bg-opacity-90 dark:focus:bg-opacity-100 dark:text-black transition-all dark:outline-none"
@@ -57,6 +141,8 @@ export default function Contact() {
           required
           maxLength={500}
           placeholder="Your email"
+          value={senderEmail}
+          onChange={(e) => setSenderEmail(e.target.value)}
         />
         <textarea
           className="h-52 my-3 rounded-lg borderBlack p-4 dark:bg-white dark:bg-opacity-90 dark:focus:bg-opacity-100 dark:text-black transition-all dark:outline-none"
@@ -64,6 +150,8 @@ export default function Contact() {
           placeholder="Message"
           required
           maxLength={5000}
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
         />
         <SubmitBtn />
       </form>
