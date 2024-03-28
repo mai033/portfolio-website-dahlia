f7be71dc03f1f09de1b46c63397ba23a25d1ab11
mai033 <145622463+mai033@users.noreply.github.com>
2024-03-27 22:37:54 -0700

    changed dashboard

--- lib/data.ts
+++ lib/data.ts
@@ -98,15 +98,6 @@ export const projectsData = [
   //   imageUrl: KubeVX,
   //   link: 'https://www.kubevx.com',
   // },
-
-  {
-    title: 'Fullstack Dashboard',
-    description:
-      'A full-stack application providing real-time monitoring of customers, transactions, and sales through a visually appealing dashboards.',
-    tags: ['React', 'Redux Toolkit', 'Node.js', 'MongoDB'],
-    imageUrl: Dashboard,
-    link: 'https://admin-frontend-ngde.onrender.com/',
-  },
   {
     title: 'Portfolio Website',
     description:
@@ -114,6 +105,14 @@ export const projectsData = [
     tags: ['React', 'Three.js'],
     imageUrl: Portfolio,
     link: 'https://dahlia-website-threejs.vercel.app/',
+  },
+  {
+    title: 'Fullstack Dashboard',
+    description:
+      'A full-stack application providing real-time monitoring of customers, transactions, and sales through a visually appealing dashboards',
+    tags: ['React', 'Redux Toolkit Query', 'Node.js', 'MongoDB'],
+    imageUrl: Dashboard,
+    link: 'https://admin-frontend-ngde.onrender.com/',
   },
   {
     title: 'Meeting Scheduler',
